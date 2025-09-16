ce3092e1a31eb4e33ddbe175f5674be64dc8b4b7
BAE-Jungwoo <113417378+BAE-Jungwoo@users.noreply.github.com>
2025-09-16 17:03:55 +0900

    블랙잭.cc

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-	int total_card;
-	int target;
+	int total_card; // 전체 카드 수
+	int target; // 목표 숫자
 	cin >> total_card >> target;
 
-	int* card = new int[total_card];
-	int sum = 0;
+	int* card = new int[total_card]; // 카드 배열
+	int sum = 0; // 3장의 카드 합
 
 	for (int i = 0; i < total_card; i++) {
 		cin >> card[i];
@@ -17,7 +17,7 @@ int main() {
 		for(int k = j + 1; k < total_card; k++){
 			for(int l = k + 1; l < total_card; l++){
 				int temp_sum = card[j] + card[k] + card[l];
-				if(temp_sum <= target && temp_sum > sum){
+				if (temp_sum <= target && temp_sum > sum) { // 3장의 카드 합이 목표 숫자 이하이고, 기존 합보다 클 때
 					sum = temp_sum;
 				}
 			}

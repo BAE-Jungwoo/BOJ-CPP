f2adce2107a64ddd874de8a3339a6911e247c1e2
BAE-Jungwoo <113417378+BAE-Jungwoo@users.noreply.github.com>
2025-09-17 14:48:15 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
 
 int main() {
-	int r = 31, sum = 0;
-
-	int L; // 문자열 길이
+	int r = 31, L;
+	long long sum = 0, M = 1234567891, power = 1; // 해시 값
 	cin >> L;
+
 	string S; // 문자열
 	cin >> S;
 
 	for (int i = 0; i < L; i++) {
-		S[i] = S[i] - 'a' + 1; // a를 1으로 맞춤
-		sum += S[i] * pow(r, i); // 해시 함수
+		int val = S[i] - 'a' + 1; // a를 1으로 맞춤
+		sum += (val * (power % M)) % M; // 해시 함수
+		power = (power * r) % M; // r의 i제곱
 	}
+	sum %= M; 
+
 	cout << sum << endl;
 	return 0;
 }

c02e41cc0a2306d3b43d79e55b1380476631cac5
BAE-Jungwoo <113417378+BAE-Jungwoo@users.noreply.github.com>
2025-09-21 00:26:23 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.cc
+++ 백준/Bronze/14626. ISBN/ISBN.cc
@@ -3,12 +3,16 @@ using namespace std;
 
 int main() {
 	char arr[14];
-	int m, check = 0, sum = 0;
+	int weight, m, check = 0, sum = 0;
 	cin >> arr;
 
 	for (int i = 0; i < 13; i++) {
 		if (arr[i] == '*') {
 			check = i; // 훼손된 위치 저장
+			if (check % 2 == 1) 
+				weight = 3;
+			else 
+				weight = 1;
 			continue;
 		}
 		else if (i % 2 == 1) {
@@ -19,21 +23,10 @@ int main() {
 		sum += arr[i] - '0';
 	}
 	
-	if(check % 2 == 1) { // 훼손된 위치일 때
-		for (m = 0; m < 10; m++) {
-			if ((sum + 3 * m) % 10 == 0) {
-				cout << m << endl;
-				break;
-			}
+	for (m = 0; m < 10; m++) {
+		if ((sum + weight * m) % 10 == 0) {
+			cout << m << endl;
+			return 0;
 		}
 	}
-	else { // 훼손된 위치가 아닐 때
-		for (m = 0; m < 10; m++) {
-			if ((sum + m) % 10 == 0) {
-				cout << m << endl;
-				break;
-			}
-		}
-	}
-	return 0;
 }

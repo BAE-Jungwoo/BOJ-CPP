6c649acf103a379f41853298128377b982cf595c
BAE-Jungwoo <113417378+BAE-Jungwoo@users.noreply.github.com>
2025-09-23 17:05:38 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 108 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Point {
+	int x;
+	int y;
+};
+
+bool compare(Point& a, Point& b) {
+	if (a.y == b.y) {
+		return a.x < b.x;
+	}
+	return a.y < b.y;
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<Point> v;
+
+	for (int i = 0; i < N; i++) {
+		Point p;
+		cin >> p.x >> p.y;
+		v.push_back(p);
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	for (const auto& p : v) {
+		cout << p.x << " " << p.y << "\n";
+	}
+	return 0;
+}

71395be8eb0999f8e56d12edff9d1d0520a08acc
BAE-Jungwoo <113417378+BAE-Jungwoo@users.noreply.github.com>
2025-09-23 16:13:29 +0900

    [Silver V] Title: 나이순 정렬, Time: 72 ms, Memory: 11260 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -4,8 +4,17 @@
 #include <algorithm>
 using namespace std;
 
-bool compare(pair<int, string> a, pair<int, string> b) {
-		return a.first < b.first;
+struct Member {
+	int age;
+	string name;
+	int order;
+};
+
+bool compare(Member& a, Member& b) {
+	if (a.age == b.age) {
+		return a.order < b.order; // 나이가 같으면 가입순서대로
+	}
+	return a.age < b.age; // 나이순으로 정렬
 }
 
 int main() {
@@ -13,17 +22,20 @@ int main() {
 	string name;
 	cin >> N;
 
-	vector<pair<int, string>> v;
+	vector<Member> v;
 
 	for (int i = 0; i < N; i++) {
-		cin >> age >> name;
-		v.push_back(make_pair(age, name));
+		Member m;
+		cin >> m.age >> m.name;
+		m.order = i; // 가입 순서
+		v.push_back(m);
 	}
 
-	stable_sort(v.begin(), v.end(), compare);
+	// stable_sort(v.begin(), v.end(), compare); // Merge sort로 한번에 가능
+	sort(v.begin(), v.end(), compare); 
 
-	for(int j = 0; j < N; j++) {
-		cout << v[j].first << " " << v[j].second << "\n";
+	for (const auto& m : v) {
+		cout << m.age << " " << m.name << "\n";
 	}
 
 	return 0;
